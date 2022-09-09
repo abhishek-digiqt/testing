1c939edf96ca26922d48f91917b736e86ed03e31
bhavik63 <90383822+bhavik63@users.noreply.github.com>
2022-09-09 10:52:30 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo docker run -d --name newman-script --restart=on-failure:5 --network=host newman-script
+sudo docker run -p 4044:4044 -d --name newman-script --restart=on-failure:5 --network=host newman-script
